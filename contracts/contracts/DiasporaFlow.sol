05cab9f4513f5b4b0887b360308ae54d4a694202
sanmipaul <sanmipaul1@gmail.com>
2026-05-14 21:01:39 +0100

    feat: define contract with cUSD reference and fee constants

--- contracts/contracts/DiasporaFlow.sol
+++ contracts/contracts/DiasporaFlow.sol
@@ -4,3 +4,10 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract DiasporaFlow is Ownable, ReentrancyGuard {
+    IERC20 public immutable cUSD;
+
+    uint256 public constant FEE_BPS = 30; // 0.3%
+    uint256 public constant BPS_DENOMINATOR = 10000;
+}
